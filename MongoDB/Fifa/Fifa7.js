a85afbbf5616eae6114a96c5d9dd7a5d6782f640
jageradrienn <jager.adrienn@gmail.com>
2023-07-11 18:46:56 +0200

    javítás

--- MongoDB/Fifa/Fifa7.js
+++ MongoDB/Fifa/Fifa7.js
@@ -1,12 +1,15 @@
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb+srv://sqladmin:T11admin$@cluster0.scj0fki.mongodb.net/T11"
 
+
+
 async function adatLekerdezes() {
     try {
         const client = await MongoClient.connect(url);
         const db = client.db('FifaAdatbazis');
         const collection = db.collection('Fifa');
-        const result = await collection.find({Valtozas:{$lt: -1}}, {projection:{_id:0, Csapat:1, Valtozas:1}}).toArray();
+        var rendezes={ Valtozas: 1 };
+        const result = await collection.find().sort(rendezes).limit(1).toArray();
         console.log(result);
 
         client.close();
